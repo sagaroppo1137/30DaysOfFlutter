c8bf0914cb04127ed8fd4b2775b9c91db7f3ce95
Sagar Patel <sagaroppo1137@gmail.com>
2021-08-24 12:38:42 +0530

    day 13 completed.

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:test1/Models/catalog.dart';
 import 'package:test1/widgets/drawer.dart';
 import 'package:test1/widgets/item_widget.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Sagar";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {
